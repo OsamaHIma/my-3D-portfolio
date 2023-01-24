d10b3c8985bdf94405c5be384fa82459706f3022
Osama magdy <108794970+OsamaHIma@users.noreply.github.com>
2023-01-24 19:34:02 +0200

    add

--- main.js
+++ main.js
@@ -6,26 +6,30 @@ const experience = new Experience(document.querySelector(".experience-canvas"));
 const fileInput = document.getElementById("file");
 const fileName = document.getElementById("file-name");
 fileInput.addEventListener("change", (ev) => {
-    fileName.textContent = ev.target.files[0].name;
-})
+  fileName.textContent = ev.target.files[0].name;
+})(
+  // Validate inputs
 
-// Validate inputs
+  () => {
+    "use strict";
 
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-const forms = document.querySelectorAll(".needs-validation");
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
 
-// Loop over them and prevent submission
-Array.from(forms).forEach((form) => {
-  form.addEventListener(
-    "submit",
-    (event) => {
-      if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
 
-      form.classList.add("was-validated");
-    },
-    false
-  );
-});
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  }
+)();
